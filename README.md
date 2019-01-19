0bb30f1340301f179e322cd2d453e014ca6597bd
icecola <jialiang.fan@shanbay.com>
2019-01-19 16:13:02 +0800

    added: - 增加 gitignore - 更新依赖 - 格式化代码 - 修改名称

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # first_project
 
-django 1.9.7
-
-有一个显示文章的页面
+显示文章的页面
 尝试了get和post以及模板类的使用
--- manage.py
+++ manage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
-#coding=utf-8
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myweb.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
 
     from django.core.management import execute_from_command_line
 
